84ff835629cb70b0440b4ef138d1aea287dd3f6c
Proloycb <proloychacroborty@gmail.com>
2022-05-11 20:37:57 +0600

    contact us updated

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import appointment from '../../assets/images/appointment.png';
+import PrimaryButton from '../Shared/PrimaryButton';
 
 const ContactUs = () => {
     return (
-        <section style={{
-            background: `url(${appointment})`
-        }} className='my-24'>
-            <div className="hero min-h-screen">
-                <div>
-                    <h4 className='text-xl text-center text-primary font-bold'>Contact Us</h4>
-                    <h2 className='text-3xl text-white'>Stay connected with us</h2>
-                    <div>
-                        <div className="card-body">
-                            <div className="form-control">
-                                <input type="text" placeholder="Email Address" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <input type="text" placeholder="Subject" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <textarea placeholder="Your message" className="input input-bordered" />
-                            </div>
-                            <div className="mt-6 text-center">
-                                <button className="btn btn-primary text-white">Submit</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <div style={{
+            background:`url(${appointment})`
+        }} className='bg-primary px-10 py-14 '>
+          <div className='text-center pb-14 text-white'>
+            <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
+              Contact Us
+            </p>
+            <h1 className='text-4xl'>Stay connected with us</h1>
+          </div>
+          <div className='grid grid-cols-1 justify-items-center gap-5'>
+            <input
+              type='text'
+              placeholder='Email Address'
+              className='input w-full max-w-md'
+            />
+            <input
+              type='text'
+              placeholder='Subject'
+              className='input w-full max-w-md'
+            />
+            <textarea
+              className='textarea w-full max-w-md'
+              placeholder='Your message'
+              rows={6}
+            ></textarea>
+            <PrimaryButton>Submit</PrimaryButton>
+          </div>
+        </div>
     );
 };
 
