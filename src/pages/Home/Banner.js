5c5b8d3ba5458d08bcf8c550657208a09123c357
Proloycb <proloychacroborty@gmail.com>
2022-05-11 17:19:37 +0600

    dental care and makeappoinment

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png';
+import bgChair from '../../assets/images/bg.png';
+import PrimaryButton from '../Shared/PrimaryButton';
 const Banner = () => {
     return (
-        <div class="hero min-h-screen px-12">
-            <div class="hero-over"></div>
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary text-white uppercase font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+        <section style={{
+            background: `url(${bgChair})`
+        }}>
+            <div class="hero min-h-screen">
+                <div class="hero-over"></div>
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <PrimaryButton>Get Started</PrimaryButton>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard img={clock} bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours"/>
             <InfoCard img={marker} bgClass="bg-accent" cardTitle="Visit our location"/>
             <InfoCard img={phone} bgClass="bg-gradient-to-r from-primary to-secondary" cardTitle="Contact us now"/>
