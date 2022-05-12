4f72f3a6b95ba04cf762b8a14545c06049812d31
Proloycb <proloychacroborty@gmail.com>
2022-05-13 00:24:57 +0600

    sign up page and require auth implement

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from './pages/About/About';
 import Appointment from './pages/Appointment/Appointment';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import RequireAuth from './pages/Login/RequireAuth';
+import SignUp from './pages/Login/SignUp';
 import Footer from './pages/Shared/Footer/Footer';
 import Navbar from './pages/Shared/Navbar/Navbar';
 
@@ -13,9 +15,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/appointment' element={<Appointment />} />
+        <Route path='/appointment' element={
+          <RequireAuth>
+            <Appointment />
+          </RequireAuth>
+        } />
         <Route path='/about' element={<About />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
       <Footer />
     </div>
