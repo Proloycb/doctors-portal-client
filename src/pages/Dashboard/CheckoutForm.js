ffecce9954b943a8292b61a8f1a71688a25ddc87
Proloycb <proloychacroborty@gmail.com>
2022-05-18 13:42:40 +0600

    payment system implement

---
+++ src/pages/Dashboard/CheckoutForm.js
@@ -0,0 +1,138 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
+import Loading from '../Shared/Loading/Loading';
+
+const CheckoutForm = ({ appointment }) => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [cardError, setCardError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [transactionId, setTransactionId] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [clientSecret, setClientSecret] = useState('');
+
+    const { _id, price, patient, patientName } = appointment;
+
+    useEffect(() => {
+        fetch('https://dry-earth-19840.herokuapp.com/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+            })
+    }, [price]);
+
+    // if(processing){
+    //     return <Loading/>
+    // }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        setCardError(error?.message || '');
+        setSuccess('')
+        setProcessing(true);
+        // confirm payment method
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: patient
+                    },
+                },
+            },
+        );
+
+        if (intentError) {
+            setCardError(intentError?.message);
+            setSuccess('');
+            setProcessing(false);
+        }
+        else {
+            setCardError('');
+            setTransactionId(paymentIntent.id)
+            console.log(paymentIntent)
+            setSuccess('Congrats! Your payment is completed');
+
+            // store payment on database
+            const payment = {
+                appointment: _id,
+                transactionId: paymentIntent._id
+            }
+            //
+            fetch(`https://dry-earth-19840.herokuapp.com/booking/${_id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment)
+            })
+            .then(res => res.json())
+            .then(data => {
+                setProcessing(false);
+                console.log(data)
+            })
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className='btn btn-success btn-xs mt-2' type="submit" disabled={!stripe || !clientSecret || success}>
+                Pay
+            </button>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+            {
+                success && <div className='text-green-500'>
+                    <p>{success}</p>
+                    <p>Your transaction id:<span className='text-orange-500 font-bold'>{transactionId}</span></p>
+                </div>
+            }
+        </form>
+    );
+};
+
+export default CheckoutForm;
--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -1,11 +1,16 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L0eRwKEErB9NVHS5sUdXuwmf4krkFKDHJ9qn8A8QbYMMUuROH996IbjMP9Jo78ajuoReiARt5gZ1nvX5MpSiRzT00h0BJrOWY');
 
 const Payment = () => {
     const { id } = useParams();
-    const url = `http://localhost:5000/booking/${id}`;
+    const url = `https://dry-earth-19840.herokuapp.com/booking/${id}`;
 
     const { data: appointment, isLoading } = useQuery(['appointment', id], () => fetch(url, {
         headers: {
@@ -28,7 +33,9 @@ const Payment = () => {
             </div>
             <div class="card w-50 max-w-md bg-base-100 shadow-xl">
                 <div class="card-body">
-                    
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm appointment={appointment} />
+                    </Elements>
                 </div>
             </div>
         </div>
